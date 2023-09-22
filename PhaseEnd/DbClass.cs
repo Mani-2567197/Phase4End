4bf0f5f661e14eb60bffcefe8675233197973a98
unknown <manikanta.official1408@gmail.com>
2023-09-22 08:57:51 +0530

    initial commit

---
+++ PhaseEnd/DbClass.cs
@@ -0,0 +1,19 @@
+﻿using PhaseEnd.Models;
+
+namespace PhaseEnd
+{
+    public class DbClass
+    {
+        static Phase4DbContext db;
+        public DbClass()
+        {
+            db = new Phase4DbContext();
+        }
+        public UserInfo AddUser(UserInfo userInfo)
+        {
+            db.UserInfos.Add(userInfo);
+            db.SaveChanges();
+            return userInfo;
+        }
+    }
+}
---
+++ PhaseEnd/TestManagement.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using PhaseEnd.Models;
+
+namespace PhaseEnd
+{
+    [TestFixture]
+    public class TestManagement
+    {
+        DbClass dbcontext;
+        [SetUp]
+        public void SetUp()
+        {
+            dbcontext = new DbClass();  
+        }
+        [Test]
+        public void AddUserTest()
+        {
+            var userInfo = new UserInfo()
+            {
+                UserId = 105,
+                Email="lokesh@gmail.com",
+                Password="lokesh@123"
+            };
+            var adding=dbcontext.AddUser(userInfo);
+            Assert.IsNotNull(adding);
+            Assert.IsTrue(adding.Equals(userInfo));
+        }
+
+
+    }
+}
